47af867e11259b38c871b923dc3b86e3c8357463
NongMinty <nutsuke.m@gmail.com>
2018-05-12 14:26:50 +0700

    budget: แก้ไข interface ประวัติการตัดงบ

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:3021',
   docUrl: 'http://203.157.156.69/api/document',
-  loginUrl: 'http://203.157.156.69/api/um/',
+  loginUrl: 'http://203.157.103.123/api/um/',
   portalUrl: 'http://203.157.156.69/api/portal',
   pmPrefix: 'PM',
 
